d9f9c92ecb68e451d9339631523cadcb552441bf
Gerrit Jansen van Vuuren <gerritjvv@gmail.com>
2020-02-13 15:22:00 +0100

    disable div 100 hack for test

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"flag"
+	"fmt"
 	"github.com/gerritjvv/tcpshaper/bandwidth"
 	"github.com/rjeczalik/netxtest"
 	"log"
@@ -11,16 +12,9 @@ import (
 
 func tcpshaperListener(l net.Listener, limitGlobal, limitPerConn int) net.Listener {
 
-	ctx := context.Background()
+	fmt.Printf("tcpshaperListener: limitGlobal: %d, limitPerConn: %d\n", limitGlobal, limitPerConn)
 
-	// For the test go run main.go -count 100 -limit-conn 0
-	// The test itself calculates a range as a multiple of the connection count
-	// e.g: want = 30s * t.Limit/t.Count => 30s * (26214400 / 100)
-	//  I tried to dynamically adjust the connection rate based on the existing connection count
-	//   but this did not pass the test either, because the conn count can be 1, 10, 100 etc at any moment.
-	if limitPerConn == 0 {
-		limitPerConn = limitGlobal / 100
-	}
+	ctx := context.Background()
 
 	serverRate := bandwidth.NewRateConfig(int64(limitGlobal), limitGlobal)
 	connRate := bandwidth.NewRateConfig(int64(limitPerConn), limitPerConn)
